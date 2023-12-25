9ec241dde3224283f8ff06dda63f6a697202d131
diler546 <makar912@mail.ru>
2023-12-25 09:05:54 +0300

    first commit

---
+++ EX_test2/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace EX_test2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTime = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.textBoxPayer = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dateTime
+            // 
+            this.dateTime.Location = new System.Drawing.Point(80, 72);
+            this.dateTime.Name = "dateTime";
+            this.dateTime.Size = new System.Drawing.Size(200, 20);
+            this.dateTime.TabIndex = 0;
+            this.dateTime.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Номер";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Дата";
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Location = new System.Drawing.Point(80, 34);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNumber.TabIndex = 3;
+            // 
+            // textBoxPayer
+            // 
+            this.textBoxPayer.Location = new System.Drawing.Point(92, 114);
+            this.textBoxPayer.Name = "textBoxPayer";
+            this.textBoxPayer.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPayer.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Плательщик ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 160);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Банк плательщика";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(408, 27);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(340, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "БИК ";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(408, 80);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(340, 80);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "ИНН ";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(435, 149);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(340, 152);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = " Банк получателя ";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(444, 196);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(340, 196);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(90, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Счет получателя";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(104, 196);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(36, 196);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Сумма ";
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.Location = new System.Drawing.Point(104, 279);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(75, 23);
+            this.buttonSubmit.TabIndex = 18;
+            this.buttonSubmit.Text = "Отправить";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(231, 279);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 19;
+            this.buttonCancel.Text = "Отменить";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(18, 14);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 17);
+            this.radioButton1.TabIndex = 21;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Юрлицо";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(88, 14);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(116, 17);
+            this.radioButton2.TabIndex = 22;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "предприниматель";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(343, 106);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 37);
+            this.groupBox1.TabIndex = 23;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выбор типа ИНН";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "\"ПАО «Лучший банк»\"",
+            "\"ПАО «Главный банк»\"",
+            "\"ПАО «Замечательный банк»\""});
+            this.comboBox1.Location = new System.Drawing.Point(121, 157);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 314);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxPayer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTime);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.TextBox textBoxPayer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button buttonSubmit;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ EX_test2/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EX_test2
+{
+    public partial class Form1 : Form
+    {
+        static int number =0;
+        public Form1()
+        {
+            InitializeComponent();
+            CountNamber();
+            textBoxNumber.Enabled=false;
+            dateTime.Enabled = false;
+            comboBox1.SelectedIndex = 1;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Кнопка отмены
+        {
+            if(MessageBox.Show("Вы хотите очистить все поля?", "Подтверждение", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                ClearDate();
+                EnableOrDisableDate(true);
+            }
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            if (CheckingTheCompletenessOfAllData())
+            {
+                if (CheckingForIncorrectCharactersInPayer())
+                {
+                    if (CheckingTheAmountForIncorrectCharacters())
+                    {
+                        if (MessageBox.Show("Вы хотите отправить данные?", "Подтверждение", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                        {
+                            ShowData();
+                            EnableOrDisableDate(false);
+                            CountNamber();
+
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("В поле Сумма введены неверные символы. Убедитесь что написали все правильно", "Ошибка");
+                    }
+                    
+                }
+                else
+                {
+                    MessageBox.Show("В поле Плательщик введены неверные символы. Убедитесь что написали все правильно", "Ошибка");
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Некоторые поля не заполнены.","Ошибка");
+            }
+            
+        }
+        private void ClearDate()
+        {
+            dateTime.Value= DateTime.Now;
+            comboBox1.SelectedIndex = 1;
+            textBoxPayer.Clear();
+        }
+        private void ShowData()
+        {
+            MessageBox.Show("Платежное поручение принято к исполнению .\n\n" +
+                              $"Number: {textBoxNumber.Text}\n" +
+                              $"Время: {dateTime.Value.ToShortDateString()}\n"+
+                              $"Плательщик: {textBoxPayer.Text}\n"+
+                              $"Банк: {comboBox1.Text}\n"
+                              ,"Ваши данные");
+        }
+        private void EnableOrDisableDate(bool enable)
+        {
+            //textBoxNamber.Enabled = enable;
+            //dateTime.Enabled = enable;
+            textBoxPayer.Enabled = enable;
+        }
+        private void CountNamber()
+        {
+            number++;
+            string numberString = Convert.ToString(number);
+            textBoxNumber.Text = numberString;
+        }
+        private bool CheckingTheCompletenessOfAllData()
+        {
+            return !string.IsNullOrWhiteSpace(textBoxPayer.Text);
+        }
+        private bool CheckingForIncorrectCharactersInPayer()
+        {
+            string pattern = @"^[\u0041-\u044B]*[-]?[\u0041-\u044B]*$";
+            return Regex.IsMatch(textBoxPayer.Text, pattern);
+        }
+        private bool CheckingTheAmountForIncorrectCharacters()
+        {
+            string pattern = @"^[0-9]{9}$";
+            return Regex.IsMatch(textBox8.Text, pattern);
+        }
+    }
+}
